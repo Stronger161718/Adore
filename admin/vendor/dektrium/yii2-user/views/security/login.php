37ccbbb6ca205021ea0ab109c4abdc501107375c
Stronger161718 <44900212+Stronger161718@users.noreply.github.com>
2018-11-10 19:30:36 +0800

    Adore

---
+++ admin/vendor/dektrium/yii2-user/views/security/login.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ * This file is part of the Dektrium project.
+ *
+ * (c) Dektrium project <http://github.com/dektrium>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+use dektrium\user\widgets\Connect;
+use dektrium\user\models\LoginForm;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/**
+ * @var yii\web\View $this
+ * @var dektrium\user\models\LoginForm $model
+ * @var dektrium\user\Module $module
+ */
+
+$this->title = Yii::t('user', 'Sign in');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<?= $this->render('/_alert', ['module' => Yii::$app->getModule('user')]) ?>
+<div class="login-box">
+    <div class="login-logo">
+        <a href="#"><b>Admin</b></a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="login-box-body">
+        <p class="login-box-msg">Sign in to start your session</p>
+
+        <?php $form = ActiveForm::begin([
+                    'id' => 'login-form',
+                    'enableAjaxValidation' => true,
+                    'enableClientValidation' => false,
+                    'validateOnBlur' => false,
+                    'validateOnType' => false,
+                    'validateOnChange' => false,
+                ]) ?>
+
+                <?php if ($module->debug): ?>
+                    <?= $form->field($model, 'login', [
+                        'inputOptions' => [
+                            'autofocus' => 'autofocus',
+                            'class' => 'form-control',
+                            'tabindex' => '1']])->dropDownList(LoginForm::loginList());
+                    ?>
+
+                <?php else: ?>
+
+                    <?= $form->field($model, 'login',
+                        ['inputOptions' => ['autofocus' => 'autofocus', 'class' => 'form-control', 'tabindex' => '1']]
+                    )->label('用户名') ?>
+                    
+
+                <?php endif ?>
+
+                <?php if ($module->debug): ?>
+                    <div class="alert alert-warning">
+                        <?= Yii::t('user', 'Password is not necessary because the module is in DEBUG mode.'); ?>
+                    </div>
+                <?php else: ?>
+                    <?= $form->field(
+                        $model,
+                        'password',
+                        ['inputOptions' => ['class' => 'form-control', 'tabindex' => '2']])
+                        ->passwordInput()
+                        ->label('密码') ?>
+                <?php endif ?>
+
+                <?= $form->field($model, 'rememberMe')->checkbox(['tabindex' => '3']) ?>
+
+                <?= Html::submitButton(
+                    Yii::t('user', 'Sign in'),
+                    ['class' => 'btn btn-primary btn-block', 'tabindex' => '4']
+                ) ?>
+
+                <?php ActiveForm::end(); ?>
+
+        <!-- /.social-auth-links -->
+
+    </div>
+    <!-- /.login-box-body -->
+</div><!-- /.login-box -->
